b390fdcf6f7ee8c8ca1ee9035d2b1431f48aede0
bernoussama <oussama.bernou@outlook.com>
2024-10-10 22:21:19 +0100

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    Basic annotations - list of floats
+"""
+
+
+def sum_list(a: list[float]) -> float:
+    """
+    Sums a list of integers
+    """
+    return sum(a)
