8e3f6448c6e7955d54d0cde28717ee480717a8d9
bernoussama <oussama.bernou@outlook.com>
2024-10-10 23:16:08 +0100

    update task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """
-    Basic annotations - functions
+    Basic annotations - duck typing
 """
 from typing import List, Sequence, Tuple
 
 
 def element_length(lst: Sequence) -> List[Tuple[Sequence, int]]:
+    """
+    Return a list of tuples containing the element and its length
+    """
     return [(i, len(i)) for i in lst]
