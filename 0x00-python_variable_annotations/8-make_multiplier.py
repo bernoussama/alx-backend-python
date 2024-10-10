dfe1e51014d9400270c981aba7e35d192d7ea83b
bernoussama <oussama.bernou@outlook.com>
2024-10-10 23:08:23 +0100

    task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    Basic annotations - functions
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    def multiplier_function(value: float) -> float:
+        return value * multiplier
+
+    return multiplier_function
