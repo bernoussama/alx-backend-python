96330fdf00679cfcfd67c2a05dc5f25971efd1ba
bernoussama <oussama.bernou@outlook.com>
2024-10-16 23:34:25 +0100

    fix: formatting

--- 0x02-python_async_comprehension/1-main.py
+++ 0x02-python_async_comprehension/1-main.py
@@ -2,7 +2,8 @@
 
 import asyncio
 
-async_comprehension = __import__("1-async_comprehension").async_comprehension
+async_comprehension = __import__(
+    "1-async_comprehension").async_comprehension
 
 
 async def main():
