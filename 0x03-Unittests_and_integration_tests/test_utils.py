18c455b136e7bc324205838cf897b843da911472
bernoussama <oussama.bernou@outlook.com>
2024-11-08 23:39:28 +0100

    update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''the utils module unit tests'''
+"""the utils module unit tests"""
 
 import unittest
 from parameterized import parameterized
@@ -14,25 +14,17 @@ from typing import (
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    '''Parameterize a unit test
-    '''
+    """Parameterize a unit test"""
 
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-    ])
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+        ]
+    )
     def test_access_nested_map(
-            self,
-            nested_map: Mapping,
-            path: Sequence,
-            expected: Union[Dict, int]
+        self, nested_map: Mapping, path: Sequence, expected: Union[Dict, int]
     ) -> None:
-        '''Tests `access_nexted_map`'s output
-        '''
+        """Tests `access_nexted_map`'s output"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
-
-    @parameterized.expand([
-        ({}, ("a",), KeyError),
-        ({"a": 1}, ("a", "b"), KeyError),
-    ])
